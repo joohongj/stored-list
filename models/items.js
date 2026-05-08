4f8f83dc548fb8722a824338760c3bb1a9d09dfa
Joohong Jung <joohongj@gmail.com>
2026-05-07 20:00:16 -0400

    update for quotes setting

--- models/items.js
+++ models/items.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
   name: String,
-  favoriteFood: String,
-  isInstructor: Boolean,
+  quote: String,
+  job: Boolean,
 
 });
 
